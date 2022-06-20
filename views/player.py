e4ae213272d75ca38eaf4da7407321e5a84552ce
Nobime <romeo.nobime@mailo.com>
2022-06-20 16:51:48 +0200

    modifying player.py

--- views/player.py
+++ views/player.py
@@ -1,10 +1,22 @@
 def player_form():
-    players_full_name = []
     print('Tournoi à 8 joueurs')
     print('Entrer le nom complet des joueur. exemple: Dupont Jean')
 
-    for i in range(8):
-        print('Entrer le nom complet du joueur:')
-        players_full_name.append(str(input()))
+    print('Nom:')
+    last_name = str(input())
+    print('Prénom:')
+    first_name = str(input())
+    print('Date de naissance:')
+    birth_date = str(input())
+    print('Sex:')
+    sex = str(input())
+    print('classement:')
+    rank = str(input())
 
-    return players_full_name
+    return {
+      'last_name': last_name,
+      'first_name': first_name,
+      'birth_date': birth_date,
+      'sex': sex,
+      'rank': rank
+    }
